f9858fe83bcc4be99e396dd6d440cde58b667c2b
IvanIndaia <ivan.indaia@gmail.com>
2025-06-27 00:51:34 -0300

    Update acaoMundo_Oitavas.js

--- javascript/acaoMundo_Oitavas.js
+++ javascript/acaoMundo_Oitavas.js
@@ -32,7 +32,7 @@ function apostarCopaDoMundo() {
 
   const elmnto = document.getElementById('helloa');
   elmnto.innerHTML = ''
-  
+
   let inpsB = document.getElementsByClassName('placar')
 
   let sou = document.querySelectorAll('button')[1]
@@ -53,7 +53,7 @@ function apostarCopaDoMundo() {
       plac[i] = parseInt(inpsB[i].value)
     }
 
-  
+
 
     $('#helloa').append(`<p style="text-align: center;"> 
     --------------------------- <br>
@@ -126,8 +126,11 @@ function whatsapp() {
 
   alert("AINDA NÃO FINALIZOU.. \nA aposta só será validada quando a mensagem chegar no Whatsapp!!!")
 
-  
-  let text = `${userName} - ${cellPhone} %0A%0AAPOSTA *FUTBR* %0AMUNDIAL DE CLUBES FIFA-2025%0A%0A--------------------------- %0AOitavas de Final%0A--------------------------- %0APalmeiras ${plac[0]} X ${plac[1]} Botafogo %0ABenfica ${plac[2]} X ${plac[3]} Chelsea %0AInter de Milão ${plac[4]} X ${plac[5]} Fluminense %0AManchester City ${plac[6]} X ${plac[7]} Al-Hilal %0APSG ${plac[8]} X ${plac[9]} Inter Miami %0AFlamengo ${plac[10]} X ${plac[11]} Bayern de Munique %0ABorussia Dortmund ${plac[12]} X ${plac[13]} Monterrey %0AReal Madrid ${plac[14]} X ${plac[15]} Juventus%0A`
+
+  let text = `NOME: ${userName} %0AContato: ${cellPhone} %0A%0AAPOSTA *FUTBR* %0AMUNDIAL DE CLUBES FIFA-2025%0A%0A--------------------------- %0AOitavas de Final%0A--------------------------- %0APalmeiras ${plac[0]} X ${plac[1]} Botafogo %0ABenfica ${plac[2]} X ${plac[3]} Chelsea %0AInter de Milão ${plac[4]} X ${plac[5]} Fluminense %0AManchester City ${plac[6]} X ${plac[7]} Al-Hilal %0APSG ${plac[8]} X ${plac[9]} Inter Miami %0AFlamengo ${plac[10]} X ${plac[11]} Bayern de Munique %0ABorussia Dortmund ${plac[12]} X ${plac[13]} Monterrey %0AReal Madrid ${plac[14]} X ${plac[15]} Juventus%0A`
+
+
+  // salvarEmArquivo()
 
   window.open(`https://wa.me/5519992481225?text=${text}`)
   paginaCopaDoMundo()
@@ -137,3 +140,23 @@ function paginaCopaDoMundo() {
   $(location).attr('href', '../html/oitavasDeFinal.html')
 }
 
+// fs.appendFile()
+
+// salvarEmArquivo() {
+
+//   const fs = require('fs');
+
+//   function salvarDadosNoTxtNode(caminhoArquivo, dados) {
+//     fs.writeFile(caminhoArquivo, dados, (err) => {
+//       if (err) {
+//         console.error("Erro ao escrever no arquivo:", err);
+//       } else {
+//         console.log("Dados salvos com sucesso!");
+//       }
+//     })
+//   }
+
+//   // Exemplo de uso
+//   const dadosParaSalvar = "Este é o texto a ser salvo no arquivo."
+//   salvarDadosNoTxtNode('./meuArquivo.txt', dadosParaSalvar);
+// }
